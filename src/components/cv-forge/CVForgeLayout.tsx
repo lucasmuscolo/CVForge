48d74ce76b4fd36708a2bfe14400ae93fd038aaa
Lucas Muscolo <lucasmuscolo@gmail.com>
2025-05-13 12:59:49 +0000

    bien, ahora necesito lo siguiente. Las pantallas de experiencia laboral y vista previa tienen que pode moverse independientemente. ahora el scroll de una depende de la otra

--- src/components/cv-forge/CVForgeLayout.tsx
+++ src/components/cv-forge/CVForgeLayout.tsx
@@ -10,13 +10,13 @@ export function CVForgeLayout({ inputSection, previewSection }: CVForgeLayoutPro
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-secondary">
       {/* Input Section */}
-      <div className="w-full md:w-1/2 lg:w-2/5 p-4 md:p-6 lg:p-8 bg-background shadow-lg">
+      <div className="w-full md:w-1/2 lg:w-2/5 p-4 md:p-6 lg:p-8 bg-background shadow-lg overflow-y-auto">
         {inputSection}
       </div>
 
       {/* Preview Section */}
       {/* Kept print styles as they are generally useful */}
-      <div className="w-full md:w-1/2 lg:w-3/5 p-4 md:p-6 lg:p-8 print:p-0 print:m-0 print:overflow-visible print:h-auto print:shadow-none">
+      <div className="w-full md:w-1/2 lg:w-3/5 p-4 md:p-6 lg:p-8 print:p-0 print:m-0 print:overflow-visible print:h-auto print:shadow-none overflow-y-auto">
         {previewSection}
       </div>
     </div>
